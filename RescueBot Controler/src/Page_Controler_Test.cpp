8c725ce8cca422748b08eb3ae4b8c0f7e9321bf6
okapifan <wxzegers@gmail.com>
2020-01-07 10:46:33 +0100

    Added Auto-mode toggle-button design

--- RescueBot Controler/src/Page_Controler_Test.cpp
+++ RescueBot Controler/src/Page_Controler_Test.cpp
@@ -163,6 +163,75 @@ const char* page_Controler_Test PROGMEM = R"=====(
 		margin: 0;
 		padding: 0;
 	}
+	
+	/* The switch - the box around the slider */
+	.switch {
+		position: relative;
+		display: inline-block;
+		width: 120px;
+		height: 68px;
+	}
+
+	/* Hide default HTML checkbox */
+	.switch input {
+		opacity: 0;
+		width: 0;
+		height: 0;
+	}
+
+	/* The slider */
+	.slider {
+		position: absolute;
+		cursor: pointer;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		background-color: #ccc;
+		-webkit-transition: .4s;
+		transition: .4s;
+	}
+
+	.slider:before {
+		position: absolute;
+		content: "";
+		height: 52px;
+		width: 52px;
+		left: 8px;
+		bottom: 8px;
+		background-color: white;
+		-webkit-transition: .4s;
+		transition: .4s;
+	}
+
+	input:checked + .slider {
+		background-color: #2196F3;
+	}
+
+	input:focus + .slider {
+		box-shadow: 0 0 1px #2196F3;
+	}
+
+	input:checked + .slider:before {
+		-webkit-transform: translateX(52px);
+		-ms-transform: translateX(52px);
+		transform: translateX(52px);
+	}
+
+	/* Rounded sliders */
+	.slider.round {
+		border-radius: 68px;
+	}
+
+	.slider.round:before {
+		border-radius: 50%;
+	}
+	
+	.switch-div {
+		position: absolute;
+		top: 1vh;
+		right: 1vw;
+	}
   </style>
 </head>
 
@@ -182,6 +251,13 @@ const char* page_Controler_Test PROGMEM = R"=====(
 	<div class="right-section">
 		<button class="button right-left"><strong><</strong></button>
 		<button class="button right-right"><strong>></strong></button>
+	</div>
+	<div class="switch-div">
+		<i>Auto-Mode.</i>
+		<label class="switch">
+			<input type="checkbox">
+			<span class="slider round"></span>
+		</label>
 	</div>
   </div>
   <!--<div class='wrapper'>
